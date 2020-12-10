2dd58db8159ce7bb938e60f9cdf07f1264ac333d
turkerontasci <turkerontasci@hotmail.com>
2020-12-10 17:11:16 +0300

    UI Fix

--- app/src/main/java/com/example/deneme/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/deneme/Fragments/ProfileFragment.java
@@ -3,9 +3,11 @@ package com.example.deneme.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatDelegate;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
 import com.example.deneme.R;
 
@@ -24,8 +26,22 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+
+        //Switch darkMode = (Switch) view.findViewById(R.id.switch1);
+
+        /*darkMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        });
+
+         */
+
+        return view;
     }
 
 }
