2c0e5bf87cd9bcfd49e94d4cdc10a80f0bb1d04d
turkerontasci <turkerontasci@hotmail.com>
2021-01-21 03:54:48 +0300

    Profil Sayfası Güncellendi

--- app/src/main/java/com/example/deneme/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/deneme/Fragments/ProfileFragment.java
@@ -1,27 +1,31 @@
 package com.example.deneme.Fragments;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatDelegate;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Switch;
-
+import android.widget.ImageView;
+import android.widget.TextView;
 import com.example.deneme.R;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ProfileFragment extends Fragment {
 
+    TextView userName, userStatus;
+    ImageView profileImage, statusIcon;
+    String status[] = {"Geliştirici", "Standart Üye", "Gümüş Üye", "Altın Üye"};
+    int selectedStatus;
+
+    Integer statusImage[] = {
+            R.drawable.dev_icon,
+            R.drawable.standart_member,
+            R.drawable.favorite_silver,
+            R.drawable.favorite_gold
+    };
 
     public ProfileFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,19 +33,31 @@ public class ProfileFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        selectedStatus = 0;
 
-        //Switch darkMode = (Switch) view.findViewById(R.id.switch1);
+        profileImage = view.findViewById(R.id.profilePicture);
+        profileImage.setImageResource(R.drawable.profilfoto);
 
-        /*darkMode.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-            }
-        });
+        userName = view.findViewById(R.id.userName);
+        userName.setText("Türker Öntaşçı");
 
-         */
+        userStatus = view.findViewById(R.id.userStatus);
+        statusIcon = view.findViewById(R.id.statusIcon);
+
+        if (selectedStatus == 0) {
+            statusIcon.setImageResource(statusImage[0]);
+            userStatus.setText(status[0]);
+        } else if (selectedStatus == 1) {
+            statusIcon.setImageResource(statusImage[1]);
+            userStatus.setText(status[1]);
+        } else if (selectedStatus == 2) {
+            statusIcon.setImageResource(statusImage[2]);
+            userStatus.setText(status[2]);
+        } else if (selectedStatus == 3) {
+            statusIcon.setImageResource(statusImage[3]);
+            userStatus.setText(status[3]);
+        }
 
         return view;
     }
-
 }
